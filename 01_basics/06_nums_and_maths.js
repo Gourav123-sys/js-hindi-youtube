e86283f3749bc0aa2eddf64beb8e098864008ab6
Gourav Mondal <72298448+Gourav123-sys@users.noreply.github.com>
2024-05-21 07:32:00 +0000

    math and  num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+// const score = 400.75376357
+
+// const balance = new Number(100.873297)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const anotherNumber = 132.45;
+
+// console.log(anotherNumber.toPrecision(6));
+
+// const hundereds = 1000000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+
+// ************** maths ******************* //
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,9,3,2,1,-2));
+// console.log(Math.max(4,9,3,2,1,-2));
+
+console.log(Math.random()*10+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor (Math.random() * (max-min+1)) + min)
