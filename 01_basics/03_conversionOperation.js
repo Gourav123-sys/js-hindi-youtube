70b4c40ad19c16e0321b87bcb9308c3d8f06cc17
Gourav Mondal <72298448+Gourav123-sys@users.noreply.github.com>
2024-05-21 05:24:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = true // undefined // null //"33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+let isLoggedIn = "Gourav" // "" // 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof isLoggedIn);
+
+let someNNumber = 33;
+
+let stringNumber = String(someNNumber);
+console.log(stringNumber);
+
+
