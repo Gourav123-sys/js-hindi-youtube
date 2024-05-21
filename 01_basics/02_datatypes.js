4df1dae6d1e15aca174f9861662dfa5ea118c3a6
Gourav Mondal <72298448+Gourav123-sys@users.noreply.github.com>
2024-05-21 06:00:04 +0000

    another commmit

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,13 +1,18 @@
-"use strict"; // treat all js code as newer version
+//"use strict"; // treat all js code as newer version
 
 // alert(3+3) // we are using nodejs, not browser
 
-console.log(3+3)
+// console.log(3+3)
 
-console.log("Gourav")
+// console.log("Gourav")
 
 // null => standalone value
 
-console.log(typeof "Gourav");
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof "Gourav");
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+const id = symbol('123');
+const anotherId = symbol('123');
+
+console.log(id === anotherId);
