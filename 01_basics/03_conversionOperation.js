8db2525bb31cb1f11a6617db903f0e45d85faf03
Gourav Mondal <72298448+Gourav123-sys@users.noreply.github.com>
2024-05-21 05:43:37 +0000

    comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,6 +16,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNNumber = 33;
 
 let stringNumber = String(someNNumber);
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// *********************** operations ********************** //
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Gourav";
+let str2 = " Mondal";
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log(true);
+// console.log(+true);
+// console.log("");
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
+
 
 
