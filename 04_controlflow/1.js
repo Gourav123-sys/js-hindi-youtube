9190d4bc02d6da579702f7e610a947a3de9f64d4
Gourav Mondal <72298448+Gourav123-sys@users.noreply.github.com>
2024-05-23 07:08:58 +0000

    controlflow

---
+++ 04_controlflow/1.js
@@ -0,0 +1,16 @@
+ // falsy values
+
+ // flase, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+ // except this evrything is thuthy value
+
+ // Nullish Coalescing Operator (??): null undefined
+ let val;
+//  val = 34??4;
+//  val = null??3;
+// val = undefined??13
+// val = null??undefined
+
+ console.log(val);
+
+ // Nullish coalescing operator and ternory operator are different thing
